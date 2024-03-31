e73460c8014b0268d44407d9a22e3881cb955095
Samuel Buraga <116203173+samuelburaga@users.noreply.github.com>
2024-03-31 17:39:03 +0300

    style: switch to custom thinker

--- user_interface.py
+++ user_interface.py
@@ -1,129 +1,106 @@
 import tkinter as tk
-from tkinter import ttk
-from ttkthemes import ThemedStyle
-
-
-def on_button_click():
-    print("Button clicked!")
-
-    text_input_values = [
-        initial_population_size_input.get(),
-        number_of_generations_input.get(),
-        number_of_bits_input.get(),
-        value_threshold_input.get(),
-    ]
-
-    slider_values = [
-        domain_min_slider.get(),
-        domain_max_slider.get(),
-        crossover_rate_slider.get(),
-        mutation_rate_slider.get(),
-    ]
-
-    print("Text Inputs:", text_input_values)
-    print("Sliders:", slider_values)
-
-
-root = tk.Tk()
-root.title("UI with Inputs and Sliders")
-
-style = ThemedStyle(root)
-style.set_theme("equilux")
-
-
-input_frame = ttk.Frame(root, padding="20")
-input_frame.grid(row=0, column=0, padx=10, pady=10)
-
-initial_population_size_label = ttk.Label(input_frame, text="Initial population size:")
-initial_population_size_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-initial_population_size_input = ttk.Entry(input_frame, width=10)
-initial_population_size_input.grid(row=0, column=1, padx=5, pady=5)
-
-number_of_generations_label = ttk.Label(input_frame, text="Number of generations:")
-number_of_generations_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
-number_of_generations_input = ttk.Entry(input_frame, width=10)
-number_of_generations_input.grid(row=1, column=1, padx=5, pady=5)
-
-number_of_bits_label = ttk.Label(input_frame, text="Number of bits:")
-number_of_bits_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
-number_of_bits_input = ttk.Entry(input_frame, width=10)
-number_of_bits_input.grid(row=2, column=1, padx=5, pady=5)
-
-value_threshold_label = ttk.Label(input_frame, text="Value threshold:")
-value_threshold_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
-value_threshold_input = ttk.Entry(input_frame, width=10)
-value_threshold_input.grid(row=3, column=1, padx=5, pady=5)
-
-domain_min_label = ttk.Label(input_frame, text="Domain min:")
-domain_min_label.grid(row=0, column=2, padx=5, pady=5, sticky="w")
-domain_min_slider = ttk.Scale(
-    input_frame,
-    from_=-500,
-    to=500,
-    length=150,
-    value=-10,
-    command=lambda value: domain_min_value.set(value),
+from customtkinter import (
+    CTk,
+    CTkButton,
+    CTkEntry,
+    CTkLabel,
+    set_appearance_mode,
+    set_default_color_theme,
 )
-domain_min_slider.grid(row=0, column=3, padx=5, pady=5)
-domain_min_value = tk.StringVar()
-domain_min_label = ttk.Label(input_frame, textvariable=domain_min_value)
-domain_min_label.grid(row=0, column=4, padx=5, pady=5)
-domain_min_value.set(domain_min_slider.get())
+
+set_appearance_mode("System")
+set_default_color_theme("green")
 
 
-domain_max_label = ttk.Label(input_frame, text="Domain max:")
-domain_max_label.grid(row=1, column=2, padx=5, pady=5, sticky="w")
-domain_max_slider = ttk.Scale(
-    input_frame,
-    from_=-500,
-    to=500,
-    length=150,
-    value=10,
-    command=lambda value: domain_max_value.set(value),
-)
-domain_max_slider.grid(row=1, column=3, padx=5, pady=5)
-domain_max_value = tk.StringVar()
-domain_max_label = ttk.Label(input_frame, textvariable=domain_max_value)
-domain_max_label.grid(row=1, column=4, padx=5, pady=5)
-domain_max_value.set(domain_max_slider.get())
+class App(CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title("Configuration")
+        self.geometry("500x500")
+
+        self.title_label = CTkLabel(
+            self, text="Configuration", font=("Helvetica", 16, "bold")
+        )
+        self.title_label.grid(row=0, column=0, columnspan=4, pady=(10, 20))
+
+        self.initial_population_size_label = CTkLabel(
+            self, text="Initial population size:"
+        )
+        self.initial_population_size_label.grid(row=1, column=0, padx=5, pady=5)
+        self.initial_population_size_input = CTkEntry(self, width=100)
+        self.initial_population_size_input.grid(row=1, column=1, padx=5, pady=5)
+        self.initial_population_size_input.insert(0, "8")
+
+        self.number_of_generations_label = CTkLabel(self, text="Number of generations:")
+        self.number_of_generations_label.grid(row=2, column=0, padx=5, pady=5)
+        self.number_of_generations_input = CTkEntry(self, width=100)
+        self.number_of_generations_input.grid(row=2, column=1, padx=5, pady=5)
+        self.number_of_generations_input.insert(0, "6")
+
+        self.number_of_bits_label = CTkLabel(self, text="Number of bits:")
+        self.number_of_bits_label.grid(row=3, column=0, padx=5, pady=5)
+        self.number_of_bits_input = CTkEntry(self, width=100)
+        self.number_of_bits_input.grid(row=3, column=1, padx=5, pady=5)
+        self.number_of_bits_input.insert(0, "5")
+
+        self.value_threshold_label = CTkLabel(self, text="Value threshold:")
+        self.value_threshold_label.grid(row=4, column=0, padx=5, pady=5)
+        self.value_threshold_input = CTkEntry(self, width=100)
+        self.value_threshold_input.grid(row=4, column=1, padx=5, pady=5)
+        self.value_threshold_input.insert(0, "1000")
+
+        self.domain_min_label = CTkLabel(self, text="Domain min:")
+        self.domain_min_label.grid(row=1, column=2, padx=5, pady=5)
+        self.domain_min_slider = tk.Scale(
+            self, from_=-500, to=450, orient=tk.HORIZONTAL
+        )
+        self.domain_min_slider.set(-10)
+        self.domain_min_slider.grid(row=1, column=3, padx=5, pady=5)
+
+        self.domain_max_label = CTkLabel(self, text="Domain max:")
+        self.domain_max_label.grid(row=2, column=2, padx=5, pady=5)
+        self.domain_max_slider = tk.Scale(
+            self, from_=-450, to=500, orient=tk.HORIZONTAL
+        )
+        self.domain_max_slider.set(10)
+        self.domain_max_slider.grid(row=2, column=3, padx=5, pady=5)
+
+        self.crossover_rate_label = CTkLabel(self, text="Crossover rate:")
+        self.crossover_rate_label.grid(row=3, column=2, padx=5, pady=5)
+        self.crossover_rate_slider = tk.Scale(
+            self, from_=0, to=1, resolution=0.0001, orient=tk.HORIZONTAL
+        )
+        self.crossover_rate_slider.set(0.07)
+        self.crossover_rate_slider.grid(row=3, column=3, padx=5, pady=5)
+
+        self.mutation_rate_label = CTkLabel(self, text="Mutation rate:")
+        self.mutation_rate_label.grid(row=4, column=2, padx=5, pady=5)
+        self.mutation_rate_slider = tk.Scale(
+            self, from_=0, to=1, resolution=0.0001, orient=tk.HORIZONTAL
+        )
+        self.mutation_rate_slider.set(0.01)
+        self.mutation_rate_slider.grid(row=4, column=3, padx=5, pady=5)
+
+        self.button = CTkButton(self, text="RUN", command=self.on_button_click)
+        self.button.grid(row=5, column=0, columnspan=4, pady=10)
+
+    def on_button_click(self):
+        text_input_values = [
+            self.initial_population_size_input.get(),
+            self.number_of_generations_input.get(),
+            self.number_of_bits_input.get(),
+            self.value_threshold_input.get(),
+        ]
+
+        slider_values = [
+            self.domain_min_slider.get(),
+            self.domain_max_slider.get(),
+            self.crossover_rate_slider.get(),
+            self.mutation_rate_slider.get(),
+        ]
 
 
-crossover_rate_label = ttk.Label(input_frame, text="Crossover rate:")
-crossover_rate_label.grid(row=2, column=2, padx=5, pady=5, sticky="w")
-crossover_rate_slider = ttk.Scale(
-    input_frame,
-    from_=0,
-    to=1,
-    length=150,
-    value=0.07,
-    command=lambda value: crossover_rate_value.set(value),
-)
-crossover_rate_slider.grid(row=2, column=3, padx=5, pady=5)
-crossover_rate_value = tk.StringVar()
-crossover_rate_label = ttk.Label(input_frame, textvariable=crossover_rate_value)
-crossover_rate_label.grid(row=2, column=4, padx=5, pady=5)
-crossover_rate_value.set(crossover_rate_slider.get())
-
-
-mutation_rate_label = ttk.Label(input_frame, text="Mutation rate:")
-mutation_rate_label.grid(row=3, column=2, padx=5, pady=5, sticky="w")
-mutation_rate_slider = ttk.Scale(
-    input_frame,
-    from_=0,
-    to=1,
-    length=150,
-    value=0.01,
-    command=lambda value: crossover_rate_value.set(value),
-)
-mutation_rate_slider.grid(row=3, column=3, padx=5, pady=5)
-mutation_rate_value = tk.StringVar()
-mutation_rate_label = ttk.Label(input_frame, textvariable=mutation_rate_value)
-mutation_rate_label.grid(row=3, column=4, padx=5, pady=5)
-mutation_rate_value.set(mutation_rate_slider.get())
-
-button = ttk.Button(root, text="Maximize function", command=on_button_click)
-button.grid(row=1, column=0, padx=10, pady=10)
-
-
-def run_app():
-    root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
